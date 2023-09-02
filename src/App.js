a99aa24e2670c0cf50736bac275f35c60b698a17
Zakria <zakigame199@gmail.com>
2023-09-02 12:30:28 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import { Route, Routes, useLocation } from "react-router-dom";
+import FeaturedProductsPage from "./pages/FeaturedProductsPage";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
+import Register from "./pages/Register";
+import LoginForm from "./pages/LoginForm";
+
 
 function App() {
+  // Get the current location using useLocation
+  const location = useLocation();
+
+  // Conditionally render Header and Footer based on the route path
+  const renderHeaderAndFooter = !location.pathname.startsWith("/register");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {renderHeaderAndFooter && <Header />}
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/featuredproducts" exact element={<FeaturedProductsPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+      {renderHeaderAndFooter && <Footer />}
+    </>
   );
 }
 
